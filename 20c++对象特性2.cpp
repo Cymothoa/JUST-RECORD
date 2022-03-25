42b88f34b43cbb825a5598e321a117fb80b6cffe
Cymothoa <89298775+Cymothoa@users.noreply.github.com>
2022-03-25 21:44:56 +0800

    Add files via upload

---
+++ 20c++对象特性2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+#include<string>
+
+//拷贝构造函数调用时机
+//1.使用一个已经创建完毕的对象来初始化一个新对象
+//2.值传递的方式给参数传参
+//3.以值方式返回局部对象
+int main()
+{
+    system("pause");
+
+    return 0;
+}
